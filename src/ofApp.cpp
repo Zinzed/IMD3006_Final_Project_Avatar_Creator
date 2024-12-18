ad1fa126bc0ee553985b965a47cf1cd22fab53da
sumaiyaibrahim04 <125154848+sumaiyaibrahim04@users.noreply.github.com>
2024-12-18 18:53:45 -0500

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,107 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+
+	m_gui.setup();
+
+	isReset = false;
+
+	isHeadAToneVisible = true;
+	isHeadBToneVisible = false;
+
+	//loading sprites
+	headATones_S.loadHeadATones();
+	headBTones_S.loadHeadBTones();
+	eyes_S.loadEyes();
+	eyebrows_S.loadEyebrows();
+	noses_S.loadNoses();
+	mouthes_S.loadMouthes();
+	clothes_S.loadClothes();
+	hairstyles_S.loadHairstyles();
+	accessories_S.loadAccessories();
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::draw() {
 
-}
+	//white bg
+	ofBackground(255);
 
-//--------------------------------------------------------------
-void ofApp::draw(){
+	float centerX = ofGetWidth() / 2.0;
+	float centerY = ofGetHeight() / 2.0;
 
-}
+	//drawing sprites
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+	//to ensure only one head shape is drawn at a time
+	if (isHeadAToneVisible) {
+		headATones_S.draw(headATones_S.getCurrentIndex(), centerX, centerY);
+	}
+	else {
+		headBTones_S.draw(headBTones_S.getCurrentIndex(), centerX, centerY);
+	}
+	
+	eyes_S.draw(eyes_S.getCurrentIndex(), centerX, centerY);
+	eyebrows_S.draw(eyebrows_S.getCurrentIndex(), centerX, centerY);
+	noses_S.draw(noses_S.getCurrentIndex(), centerX, centerY);
+	mouthes_S.draw(mouthes_S.getCurrentIndex(), centerX, centerY);
+	clothes_S.draw(clothes_S.getCurrentIndex(), centerX, centerY);
+	hairstyles_S.draw(hairstyles_S.getCurrentIndex(), centerX, centerY);
+	accessories_S.draw(accessories_S.getCurrentIndex(), centerX, centerY);
 
-}
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
 
-}
+	m_gui.begin();
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+	//buttons to cycle through each category 
+	if (ImGui::Button("Head A Tones", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		headATones_S.cycleHeadATones();
+		isHeadAToneVisible = true;
+		isHeadBToneVisible = false;
+	}
 
-}
+	if (ImGui::Button("Head B Tones", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		headBTones_S.cycleHeadBTones();
+		isHeadAToneVisible= false;
+		isHeadBToneVisible = true;
+	}
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+	if (ImGui::Button("Eyes", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		eyes_S.cycleEyes();
+	}
 
-}
+	if (ImGui::Button("Eyebrows", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		eyebrows_S.cycleEyebrows();
+	}
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+	if (ImGui::Button("Nose", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		noses_S.cycleNoses();
+	}
+	if (ImGui::Button("Mouth", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		mouthes_S.cycleMouthes();
+	}
+	if (ImGui::Button("Clothes", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		clothes_S.cycleClothes();
+	}
+	if (ImGui::Button("Hairstyles", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		hairstyles_S.cycleHairstyles();
+	}
 
-}
+	if (ImGui::Button("Accessories", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		accessories_S.cycleAccessories();
+	}
 
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+	/*if (ImGui::Button("Reset", ofVec2f(ImGui::GetWindowSize().x, 30.0f))) {
+		isReset = true;
+	}*/
+	m_gui.end();
 
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+	
 
 }
