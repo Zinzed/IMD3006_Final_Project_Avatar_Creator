2ec751f07ed3caaf69f0b12ba391b8f05804d1d5
sumaiyaibrahim04 <125154848+sumaiyaibrahim04@users.noreply.github.com>
2024-12-18 18:52:26 -0500

    Update ofApp.h

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,42 @@
 #pragma once
 
 #include "ofMain.h"
+#include "Sprite.h"
+#include "HeadATone.h"
+#include "HeadBTone.h"
+#include "Eyes.h"
+#include "Eyebrows.h"
+#include "Nose.h"
+#include "Mouth.h"
+#include "Clothing.h"
+#include "Hairstyle.h"
+#include "Accessory.h"
+#include "ofxImGui.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	//sprites 
+	HeadATone headATones_S;
+	HeadBTone headBTones_S;
+	Eyes eyes_S;
+	Eyebrows eyebrows_S;
+	Nose noses_S;
+	Mouth mouthes_S;
+	Hairstyle hairstyles_S;
+	Clothing clothes_S;
+	Accessory accessories_S;
+
+	ofxImGui::Gui m_gui;
+
+	bool isReset;
+	bool isHeadAToneVisible;
+	bool isHeadBToneVisible;
+
+
+	void setup();
+	void update();
+	void draw();
+
 };
