b5485314a8d0315b7673fd90755348363925963d
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 13:17:48 -0500

    Update UI.cpp

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,1 +1,76 @@
-#pragma once
+#include "UI.h"
+
+void UI::loadButtonImages()
+{
+	buttons_N.load("Naomi_Buttons.png");
+	//load other button images here
+}
+
+void UI::boolInitStates()
+{
+	showStartScreen = false;
+	showInstrucScreen = false;
+	showChoiceScreen = false;
+	showNaomisStyle = true;
+	showSumaiyasStyle = false;
+	showZinebsStyle = false;
+
+	enableStartBtns = false;
+	enableInstrucBtns = false;
+	enableChoiceBtns = false;
+	enableNaomiBtns = true;
+	enableSumaiyaBtns = false;
+	enableZinebBtns = false;
+}
+
+void UI::setBoundingBoxes()
+{
+	//General button bounding boxes
+
+
+	//Naomi button bounding boxes
+	backBtn_BB_N.set(70, 55, 100, 100);
+	headAToneBtn_BB_N.set(70, 265, 375, 75);
+	headBToneBtn_BB_N.set(550, 265, 375, 75);
+	eyesBtn_BB_N.set(70, 420, 375, 75);
+	eyebrowsBtn_N.set(550, 420, 375, 75);
+	noseBtn_N.set(70, 575, 375, 75);
+	mouthBtn_N.set(550, 575, 375, 75);
+	hairstyleBtn_N.set(70, 730, 375, 75);
+	clothesBtn_N.set(550, 730, 375, 75);
+	accessoriesBtn_N.set(315, 885, 375, 75);
+
+	//Sumaiya button bounding boxes
+
+
+	//Zineb button bounding boxes
+}
+
+void UI::drawNaomiStyleBtns()
+{
+	buttons_N.draw(0, 0);
+}
+
+void UI::mousePressed(int x, int y, int button)
+{
+	if (backBtn_BB_N.inside(x, y) && enableNaomiBtns)
+	{
+		showStartScreen = true;
+		showInstrucScreen = false;
+		showChoiceScreen = false;
+		showNaomisStyle = false;
+		showSumaiyasStyle = false;
+		showZinebsStyle = false;
+
+		enableStartBtns = true;
+		enableInstrucBtns = false;
+		enableChoiceBtns = false;
+		enableNaomiBtns = false;
+		enableSumaiyaBtns = false;
+		enableZinebBtns = false;
+	}
+	if (headAToneBtn_BB_N.inside(x, y) && enableNaomiBtns)
+	{
+		//headAControl.cycleHeadATones();
+	}
+}
