fa4bc36556e9e2ac9106d8809c51040bf6af684c
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 21:55:59 -0500

    Update Clothing.cpp

--- src/Clothing.cpp
+++ src/Clothing.cpp
@@ -1,9 +1,19 @@
 #include "Clothing.h"
 
-void Clothing::loadClothes() {
-    loadSprite("Sumaiya_Sprites/clothing", ".png");
+//void Clothing::loadClothes() {
+//    loadSprite("Sumaiya_Sprites/clothing", ".png");
+//}
+
+ofImage* Clothing::getClothes_N()
+{
+    return clothes_N;
 }
 
-void Clothing::cycleClothes() {
-    cycleSprites();
+void Clothing::loadClothes_N()
+{
+    loadSpriteAlt(clothes_N, 7, "Naomi_Sprites/clothes_", ".png");
+}
+
+void Clothing::cycleClothes(int maxIndex) {
+    cycleSprites(maxIndex);
 }
