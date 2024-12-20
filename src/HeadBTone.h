fcd2bf511bca008a57b512cb2a7abdb3159dd906
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 22:01:18 -0500

    Update HeadBTone.h

--- src/HeadBTone.h
+++ src/HeadBTone.h
@@ -1,13 +1,27 @@
 #pragma once
 #include "Sprite.h"
-class HeadBTone :
-    public Sprite
+class HeadBTone : public Sprite
 {
-protected:
-    int currentHeadBToneIndex = 0;
-public:
-    HeadBTone() : Sprite(8) {} // initialize with 8 sprites
-    void loadHeadBTones();
-    void cycleHeadBTones();
+    private:
+        //sprite arrays
+        ofImage headBTone_N[8];
+        //ofImage headBTone_S[8];
+        //ofImage headBTone_Z[8];
+
+    public:
+        HeadBTone() : Sprite(8) {} // initialize with 8 sprites
+        //void loadHeadBTones();
+
+        //getter functions for arrays
+        ofImage* getHeadBTones_N();
+        //ofImage* getHeadBTones_S();
+        //ofImage* getHeadBTones_Z();
+
+        //loads sprites into arrays
+        void loadHeadBTones_N();
+        //void loadHeadBTones_S();
+        //void loadHeadBTones_Z();
+
+        void cycleHeadBTones(int maxIndex);
 };
 
