89c6e9070081f24bd57bb041b4b11cfd1fa711ac
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 21:59:32 -0500

    Update Hairstyle.h

--- src/Hairstyle.h
+++ src/Hairstyle.h
@@ -1,13 +1,28 @@
 #pragma once
 #include "Sprite.h"
-class Hairstyle :
-    public Sprite
+class Hairstyle : public Sprite
 {
-protected:
-    int currentHairstyleIndex = 0;
-public:
-    Hairstyle() : Sprite(7) {} // initialize with 7 sprites
-    void loadHairstyles();
-    void cycleHairstyles();
+    private:
+        //sprite arrays
+        ofImage Hairstyles_N[24];
+        //ofImage Hairstyles_S[?];
+        //ofImage Hairstyles_Z[?];
+
+    public:
+        Hairstyle() : Sprite(7) {} // initialize with 7 sprites
+        //void loadHairstyles();
+        
+        //getter functions for arrays
+        ofImage* getHairstyles_N();
+        //ofImage* getHairstyles_S();
+        //ofImage* getHairstyles_Z();
+     
+
+        //loads sprites into arrays
+        void loadHairstyles_N();
+        //void loadHairstyles_S();
+        //void loadHairstyles_Z();
+
+        void cycleHairstyles(int maxIndex);
 };
 
