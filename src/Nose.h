d7b90d054e2727c546c88be10fbe7a43a52680da
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 22:03:06 -0500

    Update Nose.h

--- src/Nose.h
+++ src/Nose.h
@@ -1,14 +1,27 @@
 #pragma once
 #include "Sprite.h"
-class Nose :
-    public Sprite
+class Nose : public Sprite
 {
-protected:
-    int currentNoseIndex = 0;
-public:
-    Nose() : Sprite(5) {} // initialize with 5 sprites
-    void loadNoses();
+    private:
+        //sprite arrays
+        ofImage noses_N[6];
+        //ofImage noses_S[?];
+        //ofImage noses_Z[?];
 
-    void cycleNoses();
+    public:
+        Nose() : Sprite(5) {} // initialize with 5 sprites
+        //void loadNoses();
+
+        //getter functions for arrays
+        ofImage* getNoses_N();
+        //ofImage* getNoses_S();
+        //ofImage* getNoses_Z();
+
+        //loads sprites into arrays
+        void loadNoses_N();
+        //void loadNoses_S();
+        //void loadNoses_Z();
+
+        void cycleNoses(int maxIndex);
 };
 
