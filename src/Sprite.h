bafa46ba122eca9461dd25f7a5c37a73101d1158
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 22:04:24 -0500

    Update Sprite.h

--- src/Sprite.h
+++ src/Sprite.h
@@ -6,18 +6,21 @@ class Sprite
 
 protected:
     ofImage* spriteArray; // pointer to dynamically allocated array
-    int arraySize;        // sprite array size
+    
+public:
+    int arraySize = 0;        // sprite array size
     int currentIndex = 0; // tracks current sprite index
 
-public:
     Sprite(int size); // constructor
     virtual ~Sprite(); // destructor for cleanup
 
-    void loadSprite(const string& imagePath, const string& fileType);
-    virtual void draw(int index, float x, float y);
-    void cycleSprites();
-    int getCurrentIndex() const;
-    void setCurrentIndex(int index);
+    //void loadSprite(const string& imagePath, const string& fileType);
+    void loadSpriteAlt(ofImage spriteArray[], int arrayMax, string imagePath, string fileType);
+    //virtual void draw(int index, float x, float y);
+    void cycleSprites(int maxIndex);
+    void drawSprites(ofImage spriteArray[], int maxIndex, float xPos, float yPos);
+    /*int getCurrentIndex() const;
+    void setCurrentIndex(int index);*/
 };
 //private:
 //
