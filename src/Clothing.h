7e610a7175124eb3bbd741df733642595224eaf2
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 21:56:23 -0500

    Update Clothing.h

--- src/Clothing.h
+++ src/Clothing.h
@@ -1,13 +1,27 @@
 #pragma once
 #include "Sprite.h"
-class Clothing :
-    public Sprite
+class Clothing : public Sprite
 {
-protected:
-    int currentClothingIndex = 0;
-public:
-    Clothing() : Sprite(8) {} // initialize with 8 sprites
-    void loadClothes();
-    void cycleClothes();
+    private:
+        //sprite arrays
+        ofImage clothes_N[7];
+        //ofImage clothes_S[?];
+        //ofImage clothes_Z[?];
+
+    public:
+        Clothing() : Sprite(8) {} // initialize with 8 sprites
+        //void loadClothes();
+
+        //getter functions for arrays
+        ofImage* getClothes_N();
+        //ofImage* getClothes_S();
+        //ofImage* getClothes_Z();
+
+        //loads sprites into arrays
+        void loadClothes_N();
+        //void loadClothes_S();
+        //void loadClothes_Z();
+
+        void cycleClothes(int maxIndex);
 };
 
