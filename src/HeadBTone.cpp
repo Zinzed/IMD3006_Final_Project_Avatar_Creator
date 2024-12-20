2cf8099962e97c9bf7e584f1208936a780dbdbaf
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 22:00:48 -0500

    Update HeadBTone.cpp

--- src/HeadBTone.cpp
+++ src/HeadBTone.cpp
@@ -1,9 +1,20 @@
 #include "HeadBTone.h"
 
-void HeadBTone::loadHeadBTones() {
-    loadSprite("Sumaiya_Sprites/HeadBTone", ".png");
+//void HeadBTone::loadHeadBTones() {
+//    loadSprite("Sumaiya_Sprites/HeadBTone", ".png");
+//}
+
+ofImage* HeadBTone::getHeadBTones_N()
+{
+    return headBTone_N;
 }
-void HeadBTone::cycleHeadBTones() {
-    cycleSprites();
+
+void HeadBTone::loadHeadBTones_N()
+{
+    loadSpriteAlt(headBTone_N, 8, "Naomi_Sprites/headShapeB_Tone", ".png");
+}
+
+void HeadBTone::cycleHeadBTones(int maxIndex) {
+    cycleSprites(maxIndex);
 }
 
