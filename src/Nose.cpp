7dbccd418cbe2648ff0055af167832b91e3d3928
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 22:02:45 -0500

    Update Nose.cpp

--- src/Nose.cpp
+++ src/Nose.cpp
@@ -1,9 +1,19 @@
 #include "Nose.h"
 
-void Nose::loadNoses() {
-    loadSprite("Sumaiya_Sprites/nose", ".png");
+//void Nose::loadNoses() {
+//    loadSprite("Sumaiya_Sprites/nose", ".png");
+//}
+
+ofImage* Nose::getNoses_N()
+{
+    return noses_N;
 }
 
-void Nose::cycleNoses() {
-    cycleSprites();
+void Nose::loadNoses_N()
+{
+    loadSpriteAlt(noses_N, 6, "Naomi_Sprites/nose_", ".png");
+}
+
+void Nose::cycleNoses(int maxIndex) {
+    cycleSprites(maxIndex);
 }
