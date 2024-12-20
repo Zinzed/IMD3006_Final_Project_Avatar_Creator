a17f9f50fff5fd20f612f4934d71eab73467c62e
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 21:54:27 -0500

    Update Accessory.cpp

--- src/Accessory.cpp
+++ src/Accessory.cpp
@@ -1,9 +1,19 @@
 #include "Accessory.h"
 
-void Accessory::loadAccessories() {
-    loadSprite("Sumaiya_Sprites/accessory", ".png");
+//void Accessory::loadAccessories() {
+//    loadSprite("Sumaiya_Sprites/accessory", ".png");
+//}
+
+ofImage* Accessory::getAccessories_N()
+{
+    return accessories_N;
 }
 
-void Accessory::cycleAccessories() {
-    cycleSprites();
+void Accessory::loadAccessories_N()
+{
+    loadSpriteAlt(accessories_N, 6, "Naomi_Sprites/accessory_", ".png");
+}
+
+void Accessory::cycleAccessories(int maxIndex) {
+    cycleSprites(maxIndex);
 }
