7dd87860cae6e45dcfd2073270f71cbaaab23ebd
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 21:57:09 -0500

    Update Eyebrows.cpp

--- src/Eyebrows.cpp
+++ src/Eyebrows.cpp
@@ -1,9 +1,20 @@
 #include "Eyebrows.h"
 
-void Eyebrows::loadEyebrows() {
-    loadSprite("Sumaiya_Sprites/eyebrows", ".png");
+//void Eyebrows::loadEyebrows() {
+//    loadSprite("Sumaiya_Sprites/eyebrows", ".png");
+//}
+
+ofImage* Eyebrows::getEyebrows_N()
+{
+    return Eyebrows_N;
 }
 
-void Eyebrows::cycleEyebrows() {
-    cycleSprites();
+void Eyebrows::loadEyebrows_N()
+{
+    loadSpriteAlt(Eyebrows_N, 7, "Naomi_Sprites/eyebrows_", ".png");
+}
+
+void Eyebrows::cycleEyebrows(int maxIndex) {
+
+    cycleSprites(maxIndex);
 }
