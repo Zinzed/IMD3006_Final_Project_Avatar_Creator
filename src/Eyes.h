27eef5301273d629db3e82c15dddc6df504f4635
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 21:58:36 -0500

    Update Eyes.h

--- src/Eyes.h
+++ src/Eyes.h
@@ -1,12 +1,25 @@
 
 #include "Sprite.h"
-class Eyes :
-    public Sprite
+class Eyes : public Sprite
 {
-protected:
-    int currentEyesIndex = 0;
-public:
-    Eyes() : Sprite(7) {} // initialize with 7 sprites
-    void loadEyes();
-    void cycleEyes();
+    private:
+        //sprite arrays
+        ofImage Eyes_N[7];
+        //ofImage Eyes_S[?];
+        //ofImage Eyes_Z[?];
+
+    public:
+        Eyes() : Sprite(8) {} // initialize with 8 sprites
+
+        //getter functions for arrays
+        ofImage* getEyes_N();
+        //ofImage* getEyes_S();
+        //ofImage* getEyes_Z();
+
+        //loads sprites into arrays
+        void loadEyes_N();
+        //void loadEyes_S();
+        //void loadEyes_Z();
+
+        void cycleEyes(int maxIndex);
 };
