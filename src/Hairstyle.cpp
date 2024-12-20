3cd9b000f8fccf17d7c76a15595d085533f50e6a
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 21:59:07 -0500

    Update Hairstyle.cpp

--- src/Hairstyle.cpp
+++ src/Hairstyle.cpp
@@ -1,9 +1,19 @@
 #include "Hairstyle.h"
 
-void Hairstyle::loadHairstyles() {
-    loadSprite("Sumaiya_Sprites/hair", ".png");
+//void Hairstyle::loadHairstyles() {
+//    loadSprite("Sumaiya_Sprites/hair", ".png");
+//}
+
+ofImage* Hairstyle::getHairstyles_N()
+{
+    return Hairstyles_N;
 }
 
-void Hairstyle::cycleHairstyles() {
-    cycleSprites();
+void Hairstyle::loadHairstyles_N()
+{
+    loadSpriteAlt(Hairstyles_N, 24, "Naomi_Sprites/hairstyle_", ".png");
+}
+
+void Hairstyle::cycleHairstyles(int maxIndex) {
+    cycleSprites(maxIndex);
 }
