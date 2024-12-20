939f0b2b71d5c0b35b5eef243b23793227c8749d
Naomi <124922019+naomileron@users.noreply.github.com>
2024-12-19 22:01:53 -0500

    Update Mouth.cpp

--- src/Mouth.cpp
+++ src/Mouth.cpp
@@ -1,9 +1,19 @@
 #include "Mouth.h"
 
-void Mouth::loadMouthes() {
-    loadSprite("Sumaiya_Sprites/mouth", ".png");
+//void Mouth::loadMouthes() {
+//    loadSprite("Sumaiya_Sprites/mouth", ".png");
+//}
+
+ofImage* Mouth::getMouthes_N()
+{
+    return mouthes_N;
 }
 
-void Mouth::cycleMouthes() {
-    cycleSprites();
+void Mouth::loadMouthes_N()
+{
+    loadSpriteAlt(mouthes_N, 14, "Naomi_Sprites/mouth_", ".png");
+}
+
+void Mouth::cycleMouthes(int maxIndex) {
+    cycleSprites(maxIndex);
 }
